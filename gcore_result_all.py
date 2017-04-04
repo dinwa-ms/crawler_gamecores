8c6c4ba5baf403d5e730a12c963fc644e9a098e8
dwang <dwang@dwangdeMacBook-Air.local>
2017-04-03 22:33:18 -0500

    modify the output format

--- gcore_result_all.py
+++ gcore_result_all.py
@@ -4,6 +4,7 @@ count = 0
 
 with open("gcore_result_all.csv", "a+") as f:
 	writer = csv.writer(f, delimiter=",")
+	writer.writerow(["No.", "time", "title", "subtitle"])
 	for i in range (0, 9):
 		with open('gcore_result_{}.csv'.format(i), 'rb') as csvfile:
 			spamreader = csv.reader(csvfile, delimiter=',')
